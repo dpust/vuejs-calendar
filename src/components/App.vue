b64c90b4ac537f7565c8e038cd7d4022accbcad0
Dejan Pust <dejan.pust@petrol.si>
2019-05-12 14:55:45 +0200

    before Vuex

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,92 @@
 <template>
-	<div> {{ msg }}</div>
+	<div>
+		<div id="day-bar">
+			<div>Mon</div>
+			<div>Tue</div>
+			<div>Wed</div>
+			<div>Thu</div>
+			<div>Fri</div>
+			<div>Sat</div>
+			<div>Sun</div>
+		</div>
+
+		<div id="calendar">
+			<div
+			 v-for="week in weeks"
+			 class="calendar-week"
+			>
+				<calendar-day
+				 v-for="day in week"
+				 :day="day"
+				>{{ day }}</calendar-day>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import CalendarDay from './CalendarDay.vue';
 export default {
 	data () {
 		return {
-			msg: 'Hello World'
+			month: 5,
+			year: 2019
 		};
+	},
+	computed: {
+
+		days () {
+
+			//Generating all days of a month
+			let days = [];
+			let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
+			do {
+				days.push(currentDay);
+				currentDay = this.$moment(currentDay.add(1, 'days'));
+			} while ((currentDay.month() + 1) === this.month)
+
+			//Add previos days to start
+			currentDay = this.$moment(days[0]);
+
+			const SUNDAY = 0;
+			const MONDAY = 1;
+
+			if (currentDay.day() !== MONDAY) {
+				do {
+					currentDay = this.$moment(currentDay).subtract(1, 'days');
+					days.unshift(currentDay)
+				}
+				while (currentDay.day() !== MONDAY);
+
+			}
+
+			//Add days after current month
+			currentDay = this.$moment(days[days.length - 1])
+			if (currentDay.day() !== SUNDAY) {
+				do {
+					currentDay = this.$moment(currentDay).add(1, 'days');
+					days.push(currentDay)
+				} while (currentDay.day() !== SUNDAY);
+			}
+
+			return days;
+		},
+		weeks () {
+			let weeks = [];
+			let week = [];
+
+			for (let day of this.days) {
+				week.push(day);
+				if (week.length === 7) {
+					weeks.push(week);
+					week = [];
+				}
+			}
+			return weeks;
+		}
+	},
+	components: {
+		CalendarDay
 	}
 }
 </script>
---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,22 @@
+<template>
+	<div :class="classObject">{{ day.format('D') }}</div>
+</template>
+
+<script>
+export default {
+	props: ['day'],
+	computed: {
+		classObject () {
+			let Today = this.day.isSame(this.$moment(), 'day')
+			return {
+				day: true,
+				today: Today,
+				past: this.day.isSameOrBefore(this.$moment(), 'day') && !Today
+			}
+		}
+	}
+
+}
+</script>
+
+</style>
